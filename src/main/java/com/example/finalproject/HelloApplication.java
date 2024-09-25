2c4000d256876a3f3021a6e1ebe323ccc9cbc7c0
ketsuneene <c.priego.545409@umindanao.edu.ph>
2024-09-25 12:46:06 +0800

    Include next panel

--- src/main/java/com/example/finalproject/HelloApplication.java
+++ src/main/java/com/example/finalproject/HelloApplication.java
@@ -1,9 +1,13 @@
 package com.example.finalproject;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -19,24 +23,25 @@ public class HelloApplication extends Application {
             stage.setScene(scene);
             stage.show();
 
-//            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(4), e -> switchToMenuPanel(stage)));
-//            timeline.setCycleCount(1);
-//            timeline.play();
+            Timeline timeline;
+            timeline = new Timeline(new KeyFrame(Duration.seconds(4), e -> switchToMenuPanel(stage)));
+            timeline.setCycleCount(1);
+            timeline.play();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-//    private void switchToMenuPanel(Stage stage){
-//        try{
-//            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
-//            Parent menupanel = fxmlLoader.load();
-//            Scene scene = new Scene(menupanel);
-//            stage.setScene(scene);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        }
-//    }
+    private void switchToMenuPanel(Stage stage){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
+            Object menupanel = fxmlLoader.load();
+            Scene scene = new Scene((Parent) menupanel);
+            stage.setScene(scene);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
 
