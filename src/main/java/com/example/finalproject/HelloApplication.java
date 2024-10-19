9c1b7b8b0eb01135ccf28d2996a18d28f7b96fa3
Juthut <j.rosalada.545470@umindanao.edu.ph>
2024-10-19 20:50:43 +0800

    TAMA NI CHA?

--- src/main/java/com/example/finalproject/HelloApplication.java
+++ src/main/java/com/example/finalproject/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.finalproject;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -11,15 +13,29 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            // Load the hello-view.fxml
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
+            Scene helloScene = new Scene(fxmlLoader.load());
             stage.setTitle("Anga University");
-
-
-            stage.setScene(scene);
+            stage.setScene(helloScene);
             stage.show();
+
+            // Pause for 2 seconds before switching to the menu scene
+            PauseTransition delay = new PauseTransition(Duration.seconds(2));
+            delay.setOnFinished(event -> {
+                // Switch to the menu.fxml after 2 seconds
+                try {
+                    FXMLLoader menuLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
+                    Scene menuScene = new Scene(menuLoader.load());
+                    stage.setScene(menuScene);
+                    stage.setResizable(false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            delay.play(); // Start the delay timer
         } catch (IOException e) {
-            e.printStackTrace();// R
+            e.printStackTrace();
         }
     }
 
